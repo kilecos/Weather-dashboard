addb2de376b4999984d52594b0141ea6b7451f14
Kilian Lecossois <kikilecossois@gmail.com>
2026-03-30 12:32:04 +0200

    build: modification lien pour images en background

--- src/utils/weatherUtils.js
+++ src/utils/weatherUtils.js
@@ -3,14 +3,14 @@ export function getWeatherInfo(code) {
     // La fonction a donc le rôle de traducteur
     // Elle reçoit le code et elle retourne un objet avec emoji et description correspondant au code
     // Elle reçoit également un background correspondant au code et à afficher sur l'app
-    if (code === 0) return { emoji: "☀️", description: "Ciel dégagé", background: "/backgrounds/bg-clear.webp" }
-    if (code <= 2) return { emoji: "⛅", description: "Partiellement nuageux", background: "/backgrounds/bg-cloudy.webp" }
-    if (code === 3) return { emoji: "☁️", description: "Couvert", background: "/backgrounds/bg-cloudy.webp" }
-    if (code <= 49) return { emoji: "🌫️", description: "Brouillard", background: "/backgrounds/bg-foggy.webp" }
-    if (code <= 59) return { emoji: "🌦️", description: "Bruine", background: "/backgrounds/bg-rainy.webp" }
-    if (code <= 69) return { emoji: "🌧️", description: "Pluie", background: "/backgrounds/bg-rainy.webp" }
-    if (code <= 79) return { emoji: "❄️", description: "Neige", background: "/backgrounds/bg-snowy.webp" }
-    if (code <= 82) return { emoji: "🌨️", description: "Averses", background: "/backgrounds/bg-rainy.webp" }
-    if (code <= 99) return { emoji: "⛈️", description: "Orage", background: "/backgrounds/bg-stormy.webp" }
+    if (code === 0) return { emoji: "☀️", description: "Ciel dégagé", background: `${import.meta.env.BASE_URL}backgrounds/bg-clear.webp` }
+    if (code <= 2) return { emoji: "⛅", description: "Partiellement nuageux", background: `${import.meta.env.BASE_URL}backgrounds/bg-cloudy.webp` }
+    if (code === 3) return { emoji: "☁️", description: "Couvert", background: `${import.meta.env.BASE_URL}backgrounds/bg-cloudy.webp` }
+    if (code <= 49) return { emoji: "🌫️", description: "Brouillard", background: `${import.meta.env.BASE_URL}backgrounds/bg-foggy.webp` }
+    if (code <= 59) return { emoji: "🌦️", description: "Bruine", background: `${import.meta.env.BASE_URL}backgrounds/bg-rainy.webp` }
+    if (code <= 69) return { emoji: "🌧️", description: "Pluie", background: `${import.meta.env.BASE_URL}backgrounds/bg-rainy.webp` }
+    if (code <= 79) return { emoji: "❄️", description: "Neige", background: `${import.meta.env.BASE_URL}backgrounds/bg-snowy.webp` }
+    if (code <= 82) return { emoji: "🌨️", description: "Averses", background: `${import.meta.env.BASE_URL}backgrounds/bg-rainy.webp` }
+    if (code <= 99) return { emoji: "⛈️", description: "Orage", background: `${import.meta.env.BASE_URL}backgrounds/bg-stormy.webp` }
     return { emoji: "🌡️", description: "Inconnu" }
 }
